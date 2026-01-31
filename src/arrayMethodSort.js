db21fba7b6072334d755eba61584471b182e970c
pavlo <nadazasada@gmail.com>
2026-01-31 15:52:11 +0200

    tests passed

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,15 +5,17 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
+    const input =
+      compareFunction || ((a, b) => (String(a) > String(b) ? 1 : -1));
+
     for (let i = 0; i < this.length; i++) {
-      for (let j = 0; j < this.length - 1; j++) {
-        const res = compareFunction(this[j], this[j + 1]);
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        const res = input(this[j], this[j + 1]);
 
         if (res > 0) {
           const buf = this[j];
 
           this[j] = this[j + 1];
-
           this[j + 1] = buf;
         }
       }
