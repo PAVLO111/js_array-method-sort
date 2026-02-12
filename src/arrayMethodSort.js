5d3f83e858e70ba26e990782478c939140709827
pavlo <nadazasada@gmail.com>
2026-02-12 23:29:41 +0200

    updated code

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,7 +6,14 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
     const input =
-      compareFunction || ((a, b) => (String(a) > String(b) ? 1 : -1));
+      compareFunction ||
+      ((a, b) => {
+        if (String(a) === String(b)) {
+          return 0;
+        } else {
+          return String(a) > String(b) ? 1 : -1;
+        }
+      });
 
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.length - 1 - i; j++) {
@@ -24,5 +31,9 @@ function applyCustomSort() {
     return this;
   };
 }
+
+Array.prototype.sort = function (compareFunction) {
+  return [].__proto__.sort2.call(this, compareFunction);
+};
 
 module.exports = applyCustomSort;
